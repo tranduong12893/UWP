e9a865e6f88cdca91ca6f328c0907ab6638a25bf
tranduong12893 <73330600+tranduong12893@users.noreply.github.com>
2021-10-29 16:49:22 +0700

    up

---
+++ t2010a_UWP/t2010a_UWP/Pages/EmailForm.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace t2010a_UWP.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class EmailForm : Page
+    {
+        public EmailForm()
+        {
+            this.InitializeComponent();
+        }
+        private void Send(object sender, RoutedEventArgs e)
+        {
+            var email = inputEmail.Text;
+            var title = inputTitle.Text;
+            var content = inputContent.Text;
+
+            txtEmail.Text = email;
+            txtTitle.Text = title;
+            txtContent.Text = content;
+        }
+    }
+}
