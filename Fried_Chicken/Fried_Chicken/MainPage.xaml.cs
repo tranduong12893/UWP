d90cd4a1fc7ecf9409dc02fd09b57e06b80cb1e0
tranduong12893 <73330600+tranduong12893@users.noreply.github.com>
2021-11-03 17:16:30 +0700

    update

--- Fried_Chicken/Fried_Chicken/MainPage.xaml.cs
+++ Fried_Chicken/Fried_Chicken/MainPage.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Fried_Chicken.Models;
+using Windows.UI.Xaml.Media.Imaging;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +27,39 @@ namespace Fried_Chicken
         public MainPage()
         {
             this.InitializeComponent();
+            MainFrame.Navigate(typeof(Pages.Home));
+            var item1 = new MenuItem() { Name = "Home", MenuPage = "homepage", Icon = new BitmapImage(new Uri("ms-appx:///Assets/icons8-windows-client-96.png")) };
+            var item2 = new MenuItem() { Name = "Eat-In", MenuPage = "eat-in", Icon = new BitmapImage(new Uri("ms-appx:///Assets/knife and fork.jpg")) };
+            var item3 = new MenuItem() { Name = "Collection", MenuPage = "customer", Icon = new BitmapImage(new Uri("ms-appx:///Assets/icons8-list-128.png")) };
+            var item4 = new MenuItem() { Name = "Delivery", MenuPage = "delivery", Icon = new BitmapImage(new Uri("ms-appx:///Assets/delivery.png")) };
+            var item5 = new MenuItem() { Name = "Take-Away", MenuPage = "take-away", Icon = new BitmapImage(new Uri("ms-appx:///Assets/pizza-slice.png")) };
+            var item6 = new MenuItem() { Name = "Payment", MenuPage = "payment", Icon = new BitmapImage(new Uri("ms-appx:///Assets/money.png")) };
+            var item7 = new MenuItem() { Name = "Customers", MenuPage = "customers", Icon = new BitmapImage(new Uri("ms-appx:///Assets/information.png")) };
+            Menu.Items.Add(item1);
+            Menu.Items.Add(item2);
+            Menu.Items.Add(item3);
+            Menu.Items.Add(item4);
+            Menu.Items.Add(item5);
+            Menu.Items.Add(item6);
+            Menu.Items.Add(item7);
+        }
+        private void IconClick_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            SP.IsPaneOpen = !SP.IsPaneOpen;
+        }
+        private void ListViewItem_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            MenuItem selected = (MenuItem)Menu.SelectedItem;
+            switch (selected.MenuPage)
+            {
+                case "homepage": MainFrame.Navigate(typeof(Pages.Home)); break;
+                case "eat-in": MainFrame.Navigate(typeof(Pages.Eat_In)); break;
+                case "collection": MainFrame.Navigate(typeof(Pages.Collection)); break;
+                case "delivery": MainFrame.Navigate(typeof(Pages.Delivery)); break;
+                case "take-away": MainFrame.Navigate(typeof(Pages.Take_Away)); break;
+                case "payment": MainFrame.Navigate(typeof(Pages.Payment)); break;
+                case "customers": MainFrame.Navigate(typeof(Pages.Customers)); break;
+            }
         }
     }
 }
---
+++ Fried_Chicken/Fried_Chicken/Models/MenuItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+
+
+namespace Fried_Chicken.Models
+{
+    class MenuItem
+    {
+        public string Name { set; get; }
+        public string MenuPage { set; get; }
+        public BitmapImage Icon { set; get; }
+
+    }
+}
